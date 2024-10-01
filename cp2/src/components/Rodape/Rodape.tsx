ca69c2d6bcc65ff0a20237bba091c4e4e10ba834
lukashibuya <uraluka@uol.com.br>
2024-09-30 23:50:29 -0300

    rodape

---
+++ cp2/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-700 text-white text-center py-4">
+      <p className="text-lg">Desenvolvido por: Gabriel, Luka e Eduardo</p>
+    </footer>
+  );
+}
